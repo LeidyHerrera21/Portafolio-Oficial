ad20c69921fec494c62bf61320433927323a8c91
leidy <leidyherrera2140@gmail.com>
2026-03-05 19:34:14 -0500

    a

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function mensaje(){
 alert("Has despertado al vampiro del código... Bienvenido a mi portafolio oscuro.");
 
 }
+
+/*animacion de habilidades*/
+
+window.addEventListener("load", function(){
+
+const barras = document.querySelectorAll(".progreso");
+
+barras.forEach(barra=>{
+let ancho = getComputedStyle(barra).width;
+barra.style.width = "0";
+
+setTimeout(()=>{
+barra.style.width = ancho;
+},300);
+
+});
+
+});
+
+
